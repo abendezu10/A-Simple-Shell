55f2b8ba26eeb601c1f77cd690a22060686ebe7d
bendazoo <abendezu385@gmail.com>
2024-09-10 17:54:08 -0400

    adding main function

--- main.c
+++ main.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 
-int main() {
+#define MAX 1024
 
+int main(int argc, char **argv ) {
 
+    int num = 0;
 
     while(1){
+
+        char command[MAX];
+        printf("$ ");
+        fgets(command, MAX, stdin);
+
+        for(int i = 0; i<sizeof(command); i++){
+
+            if(*(command+i)== ' '){
+
+
+
+
+            }
+
+
+
+        }
 
 
 
