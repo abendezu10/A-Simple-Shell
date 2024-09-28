dcfa3198e5e2615933c6001df9b704054fbbc386
bendazoo <abendezu385@gmail.com>
2024-09-28 18:32:38 -0400

    added mkdir function

--- main.c
+++ main.c
@@ -2,38 +2,74 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 void ls();
+void mkdir(char name[]); 
 
 	int main(){
 		
 		int exit = 0;
-		char *command= (char*)malloc(100 * (sizeof(char)));
+		char *command = (char*)malloc(100 * (sizeof(char)));
+		int i,j;
+		
 
 		while(exit != 1){
+			i = 0;
+			j = 0;
 
+			char *exec;
 			printf("abendezu$ ");
 			command = fgets(command, 100, stdin);
 			*(command + strlen(command) - 1) = '\0';
-			if(strcmp(command, "ls") == 0){
-				ls();
-			}
 			
-			if(strcmp(command, "exit") == 0){
+			while(*(command+i) != '\0'){
+				
+				
+				if(*(command+i) == ' '){
+					exec = (char*)calloc(i+2,sizeof(char));
+						while(j != i){
+							*(exec + j) = *(command+j);
+							j++;
+						
+							}
+						*(exec+j+1) = '\0';
+					}
+				i++;
+				}	
+			
 
+
+			if(strcmp(exec, "ls") == 0){
+				ls();
+				continue;
+
+			} else if(strcmp(exec, "mkdir") == 0){
+				mkdir();
+				continue;
+			}else if(strcmp(command, "exit") == 0){
 
 				exit = 1;
 			}
 
+
+			free(exec);
 		}
-			
+
 		free(command);
 
 		return 0;
 	}
 
+
+
 void ls(){
 
-
-
-
 	}
+
+
+void mkdir(char name){
+
+
+
+}
